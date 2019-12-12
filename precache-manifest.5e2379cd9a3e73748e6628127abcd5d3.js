d73e96b60b812b69d074064fb9157e9cc2bfa120
JoCoKo <jocoko@ya.ru>
2019-12-12 19:51:52 +0300

    Updates

---
+++ precache-manifest.5e2379cd9a3e73748e6628127abcd5d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26ca1110ea62546f075e5ef3ac988c3",
+    "url": "/WebLab2/index.html"
+  },
+  {
+    "revision": "02d5903290947e0251b0",
+    "url": "/WebLab2/static/css/main.98984755.chunk.css"
+  },
+  {
+    "revision": "2cc68894a327b80d42aa",
+    "url": "/WebLab2/static/js/2.8670cfa3.chunk.js"
+  },
+  {
+    "revision": "02d5903290947e0251b0",
+    "url": "/WebLab2/static/js/main.95d0dc23.chunk.js"
+  },
+  {
+    "revision": "49f90d1f4997bbe10cd0",
+    "url": "/WebLab2/static/js/runtime-main.e46450e8.js"
+  }
+]);
